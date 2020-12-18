87ea5cbbeb15987116bab552cdc7135b3b59f38b
AvikKole <63571638+AvikKole@users.noreply.github.com>
2020-12-18 11:59:33 +0530

    Update video filter.py

--- video filter.py
+++ video filter.py
@@ -1,32 +1,34 @@
-import numpy as np
 import cv2
-cap = cv2.VideoCapture(0)
-blue=input("Enter blue pixel intensity :")
-green=input("Enter green pixel intensity :")
-red=input("Enter red pixel intensity :")
-def verify_alpha_channel(frame):
-    try:
-        frame.shape[3] # looking for the alpha channel
-    except IndexError:
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
-    return frame
-def apply_video_filter(frame, intensity=0.5):
-    frame = verify_alpha_channel(frame)
-    frame_h, frame_w, frame_c = frame.shape
+import numpy as np
+def nothing(x):
+    pass
+cap=cv2.VideoCapture(0)
+cv2.namedWindow("filter")
+cv2.createTrackbar("Blue","filter",0,255,nothing)
+cv2.createTrackbar("Green","filter",0,255,nothing)
+cv2.createTrackbar("Red","filter",0,255,nothing)
+cv2.createTrackbar("Intensity","filter",1,10,nothing)
+def apply_video_filter(filter):
+    filter_h, filter_w, filter_c = filter.shape
     alpha=1
-    bgra = (blue,green,red,alpha)
-    overlay = np.full((frame_h, frame_w, frame_c), bgra, dtype='uint8')
-    cv2.addWeighted(overlay, intensity, frame, 1.0, 0, frame)# overlay-src1,alpha-inensity=0.8,frame=src2,beta=1.0,gamma=0,dest-frame
-    return frame
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
-    filter = apply_video_filter(frame.copy())
-    cv2.imshow('frame',frame)
-    cv2.imshow('Video_filter',filter)
-    k=cv2.waitKey(20) & 0xFF
+    bgra = (B,G,R,alpha)
+    intensity=0.1*Intensity
+    overlay = np.full((filter_h, filter_w, filter_c), bgra, dtype='uint8')#returns a new array of given shape,type & fill value
+    cv2.addWeighted(overlay, intensity, filter, 1.0, 0, filter)# overlay-src1,alpha-inensity=0.8,filter=src2,beta=1.0,gamma=0,dest-filter
+    return filter
+while True:
+    _,filter=cap.read()
+    B=cv2.getTrackbarPos("Blue","filter")
+    G=cv2.getTrackbarPos("Green","filter")
+    R=cv2.getTrackbarPos("Red","filter")
+    Intensity=cv2.getTrackbarPos("Intensity","filter")
+    filter = cv2.cvtColor(filter, cv2.COLOR_BGR2BGRA)
+    filter = apply_video_filter(filter.copy())
+    cv2.imshow('filter',filter)
+    #print(alpha)
+    k=cv2.waitKey(30) & 0xFF
     if k==27:
         break
 cap.release()
-cv2.destroyAllWindows()    
+cv2.destroyAllWindows()
+    
